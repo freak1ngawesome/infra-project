9969323564d32d4939541ce551d1185cd948382f
Danila Ivanov <deivanov@yandex-team.ru>
2022-07-16 13:48:40 +0300

    test123

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React123
+          Learn React1234
         </a>
       </header>
     </div>
